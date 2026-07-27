a06b423113fd0eb7a02436c583259357b04bdfd5
DecoderCoder <45909894+DecoderCoder@users.noreply.github.com>
2026-07-27 22:12:59 +0200

    build: scaffold Forge add-on

---
+++ src/main/java/de/decode/bluemapplayermodels/BlueMapPlayerModels.java
@@ -0,0 +1,9 @@
+package de.decode.bluemapplayermodels;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BlueMapPlayerModels.MOD_ID)
+public final class BlueMapPlayerModels {
+    public static final String MOD_ID = "bluemap_player_models";
+}
+
